a61594e584ef96a340d1e938fd0fa63f4701dbe9
lol9519 <32627694+lol9519@users.noreply.github.com>
2018-12-01 15:54:42 -0600

    arreglando error

---
+++ administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-11-30 05:00:39 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-11-30T05:00:39+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-11-30T05:00:46+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-11-30T05:00:51+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-11-30T07:07:34+00:00	INFO ::1	joomlafailure	El usuario y la clave no coinciden o no tiene una cuenta aún.
+2018-11-30T07:08:00+00:00	INFO ::1	joomlafailure	El usuario y la clave no coinciden o no tiene una cuenta aún.
---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-11-30 05:01:43 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-11-30T05:01:43+00:00	INFO ::1	update	Update started by user Super User (851). Old version is 3.9.0.
+2018-11-30T05:01:51+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.1-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20181130%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20181130T050148Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=513c41d857d962f750bca7c4842ee5420c55951ac301f9cf1b86d84233b67c6e.
