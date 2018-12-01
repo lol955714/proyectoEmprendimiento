c166080a852982945e2f5c9cb1ecb6edeb9ca1c3
lol9519 <32627694+lol9519@users.noreply.github.com>
2018-12-01 16:52:29 -0600

    update

--- administrator/logs/joomla_update.php
+++ administrator/logs/joomla_update.php
@@ -6,3 +6,11 @@
 #Fields: datetime	priority clientip	category	message
 2018-11-30T05:01:43+00:00	INFO ::1	update	Update started by user Super User (851). Old version is 3.9.0.
 2018-11-30T05:01:51+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.1-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20181130%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20181130T050148Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=513c41d857d962f750bca7c4842ee5420c55951ac301f9cf1b86d84233b67c6e.
+2018-12-01T22:37:08+00:00	INFO ::1	update	Actualización iniciada por el usuario Super User (851). La versión antigua es 3.9.0.
+2018-12-01T22:37:15+00:00	INFO ::1	update	Descargando archivo de actualización de https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.1-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20181201%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20181201T223712Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=62c2efac6f965e98beae7baf0778f2d914cf448797300dd7475b59800364c719.
+2018-12-01T22:39:16+00:00	INFO ::1	update	Archivo Joomla_3.9.1-Stable-Update_Package.zip descargado.
+2018-12-01T22:39:16+00:00	INFO ::1	update	Empezando la instalación de una nueva versión.
+2018-12-01T22:47:19+00:00	INFO ::1	update	Terminando la instalación.
+2018-12-01T22:47:19+00:00	INFO ::1	update	Borrando archivos y carpetas innecesarias.
+2018-12-01T22:47:27+00:00	INFO ::1	update	Limpieza posinstalación.
+2018-12-01T22:47:27+00:00	INFO ::1	update	Ha terminado la actualización a la versión 3.9.1.
--- configuration.php
+++ configuration.php
@@ -5,7 +5,7 @@ class JConfig {
 	public $display_offline_message = '1';
 	public $offline_image = '';
 	public $sitename = 'Simbio';
-	public $editor = 'tinymce';
+	public $editor = 'none';
 	public $captcha = '0';
 	public $list_limit = '10';
 	public $access = '1';
